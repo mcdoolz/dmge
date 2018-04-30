2bca1402367fc9db999621df744d2b63213b96d4
mcdoolz <dpouriliaee@kellett.nt.ca>
2018-04-30 15:48:04 -0700

    commented out registration javascript from mc

--- sites/all/modules/features/mc/js/mc.register.js
+++ sites/all/modules/features/mc/js/mc.register.js
@@ -1,13 +1,12 @@
-(function ($, Drupal, window, document, undefined) {
-  console.log('here');
-  Drupal.behaviors.mc_register = {
-    attach: function(context, settings) {
-      $("form#user-login").submit(function(e) {
-        fields = $('form#user-login input');
-        fields.each(function(e)) {
-          console.log(e);
-        }
-    });
-    }
-  }
-})(jQuery, Drupal, this, this.document);
+// (function ($, Drupal, window, document, undefined) {
+//   Drupal.behaviors.mc_register = {
+//     attach: function(context, settings) {
+//       $("form#user-login").submit(function(e) {
+//         fields = $('form#user-login input');
+//         fields.each(function(e)) {
+//           console.log(e);
+//         }
+//     });
+//     }
+//   }
+// })(jQuery, Drupal, this, this.document);
