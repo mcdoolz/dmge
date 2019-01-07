13c7eef2259df8c01c101e88071a11086d2aec11
mcdoolz <me@mcdoolz.com>
2019-01-07 10:36:41 -0800

    artist template semicolon fix

--- sites/all/themes/dmge/templates/field--field-artist-links--artist.tpl.php
+++ sites/all/themes/dmge/templates/field--field-artist-links--artist.tpl.php
@@ -7,7 +7,7 @@
   <div class="<?php print $classes; ?>"<?php print $attributes; ?>>
     <a href="<?php print $item['field_artist_link'][0]['#element']['url']; ?>" target="_blank">
       <i class="<?php print $item['field_artist_link_type'][0]['#options']['entity']->field_fontawesome_classname[LANGUAGE_NONE][0]['value']; ?>">&nbsp;</i>
-      <?php print (!empty($item['field_artist_link_type'])) ? $item['field_artist_link_type'][0]['#title']; ?>
+      <?php print (!empty($item['field_artist_link_type'])) ? $item['field_artist_link_type'][0]['#title'] : ''; ?>
     </a>
   </div>
 <?php endforeach; ?>
