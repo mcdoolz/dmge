269e1cdea9210a630d6847af7c34045637c8d0ba
mcdoolz <me@mcdoolz.com>
2019-01-14 23:59:33 -0800

    check if link is empty before tkrying to print

--- sites/all/themes/dmge/templates/field--field-artist-links--artist.tpl.php
+++ sites/all/themes/dmge/templates/field--field-artist-links--artist.tpl.php
@@ -4,11 +4,13 @@
 
 <div class="artist-links">
 <?php foreach ($items as $delta => $item): ?>
+  <?php if (!empty($item['field_artist_link'])) : ?>
   <div class="<?php print $classes; ?>"<?php print $attributes; ?>>
     <a href="<?php print $item['field_artist_link'][0]['#element']['url']; ?>" target="_blank">
       <i class="<?php print $item['field_artist_link_type'][0]['#options']['entity']->field_fontawesome_classname[LANGUAGE_NONE][0]['value']; ?>">&nbsp;</i>
       <?php print (!empty($item['field_artist_link_type'])) ? $item['field_artist_link_type'][0]['#title'] : ''; ?>
     </a>
   </div>
+  <?php endif; ?>
 <?php endforeach; ?>
 </div>
